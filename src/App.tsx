a219c82aad60099563a36480610b9be088a3ee6d
Tim <timji0210@gmail.com>
2019-08-25 19:30:14 +0800

    Add Button API TEST

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 const App: React.FC = () => {
   return (
@@ -10,17 +11,21 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
+        <button
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={() => {
+            axios
+              .get('/api/customer/getCustomer', { params: { id: 123 } })
+              .then(res => {
+                console.warn(res);
+              });
+          }}
         >
-          Learn React
-        </a>
+          API TEST
+        </button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
