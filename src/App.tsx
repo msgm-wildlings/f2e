95c73c76cd91bf1241284a4875e93c2df4522941
Tim <timji0210@gmail.com>
2019-09-30 22:20:46 +0800

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,86 @@
 import React, { FC, useState } from 'react';
 import './App.scss';
 import { Menu, Icon, Layout } from 'antd';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 const { Header, Sider, Content } = Layout;
 
-const App: FC = () => {
+interface AppProps {
+  pathname?: string;
+}
+const App: FC<AppProps> = ({ pathname }: AppProps) => {
   const [collapsed, setCollapsed] = useState(false);
   const toggleCollapsed = () => {
     setCollapsed(!collapsed);
   };
+  const Home = () => {
+    return <h2>Home</h2>;
+  };
+
+  const About = () => {
+    return <h2>About</h2>;
+  };
+
+  const Users = () => {
+    return <h2>Users</h2>;
+  };
+
   return (
-    <Layout style={{ height: '100vh' }}>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1">
-            <Icon type="user" />
-            <span>nav 1</span>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Icon type="video-camera" />
-            <span>nav 2</span>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Icon type="upload" />
-            <span>nav 3</span>
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout>
-        <Header style={{ background: '#fff', paddingLeft: 24 }}>
-          <Icon
-            className="trigger"
-            type={collapsed ? 'menu-unfold' : 'menu-fold'}
-            onClick={toggleCollapsed}
-          />
-        </Header>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            background: '#fff',
-            minHeight: 280
-          }}
-        >
-          Content
-        </Content>
+    <Router>
+      <Layout style={{ height: '100vh' }}>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={[pathname!]}>
+            <Menu.Item key="/">
+              <Link to="/">
+                <Icon type="user" />
+                <span>Home</span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="/about">
+              <Link to="/about">
+                <Icon type="video-camera" />
+                <span>About</span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="/users">
+              <Link to="/users">
+                <Icon type="upload" />
+                <span>Users</span>
+              </Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', paddingLeft: 24 }}>
+            <Icon
+              className="trigger"
+              type={collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={toggleCollapsed}
+            />
+          </Header>
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+              <Switch>
+                <Route path="/about">
+                  <About />
+                </Route>
+                <Route path="/users">
+                  <Users />
+                </Route>
+                <Route path="/">
+                  <Home />
+                </Route>
+              </Switch>
+            </div>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </Router>
   );
+};
+App.defaultProps = {
+  pathname: window.location.pathname
 };
 
 export default App;
