9a90c5810ed59b43279c4ba1c35fbf5d836d2455
Tim <timji0210@gmail.com>
2019-09-30 21:17:51 +0800

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,52 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './App.scss';
-import axios from 'axios';
-import { Button } from 'antd';
+import { Menu, Icon, Layout } from 'antd';
+const { Header, Sider, Content } = Layout;
 
 const App: FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
   return (
-    <div className="App">
-      <Button
-        type="primary"
-        onClick={async () => {
-          let id = 1;
-          let res = await axios.get(`/api/customer/${id}`);
-          console.warn(res);
-        }}
-      >
-        ANTD BUTTON
-      </Button>
-    </div>
+    <Layout style={{ height: '100vh' }}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">
+            <Icon type="user" />
+            <span>nav 1</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Icon type="video-camera" />
+            <span>nav 2</span>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Icon type="upload" />
+            <span>nav 3</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header style={{ background: '#fff', paddingLeft: 24 }}>
+          <Icon
+            className="trigger"
+            type={collapsed ? 'menu-unfold' : 'menu-fold'}
+            onClick={toggleCollapsed}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            background: '#fff',
+            minHeight: 280
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
 
