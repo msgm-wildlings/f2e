78c928eafa61313116b4069976ee2c5cbe659a42
Tim <timji0210@gmail.com>
2019-10-20 22:14:53 +0800

    fix yarn build at webpack.config.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -167,13 +167,13 @@ module.exports = function(webpackEnv) {
       pathinfo: isEnvDevelopment,
       // There will be one main bundle, and one file per asynchronous chunk.
       // In development, it does not produce real files.
-      filename: 'static/js/wildings.bundle.js',
+      filename: 'static/js/wildlings.bundle.js',
       //   ? 'static/js/[name].[contenthash:8].js'
       //   : isEnvDevelopment && 'static/js/bundle.js',
       // // TODO: remove this when upgrading to webpack 5
       futureEmitAssets: true,
       // There are also additional JS chunk files if you use code splitting.
-      chunkFilename: 'static/js/[name].wildings.chunk.js',
+      chunkFilename: 'static/js/[name].wildlings.chunk.js',
       //   ? 'static/js/[name].[contenthash:8].chunk.js'
       //   : isEnvDevelopment && 'static/js/[name].chunk.js',
       // // We inferred the "public path" (such as / or /my-project) from homepage.
@@ -556,7 +556,7 @@ module.exports = function(webpackEnv) {
           // Options similar to the same options in webpackOptions.output
           // both options are optional
           filename: 'static/css/[name].[contenthash:8].css',
-          chunkFilename: 'static/css/wildings.chunk.css'
+          chunkFilename: 'static/css/[name].chunk.css'
         }),
       // Generate a manifest file which contains a mapping of all asset filenames
       // to their corresponding output file so that tools can pick it up without
