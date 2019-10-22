9d6285d14baec20708a9915606ff1699351d84bc
Tim <timji0210@gmail.com>
2019-10-23 00:49:44 +0800

    會員表單第三頁 UI

--- src/components/userForm/index.tsx
+++ src/components/userForm/index.tsx
@@ -10,6 +10,7 @@ import {
   Checkbox
 } from 'antd';
 import { IUserForm } from '../interface';
+import './index.sass';
 const { Step } = Steps;
 const { Item } = Form;
 const { Group } = Checkbox;
@@ -46,7 +47,7 @@ const _UserForm: FC<IUserForm> = ({
       </Steps>
       <Divider />
       <Form onSubmit={onSummit}>
-        <Carousel ref={sliderRef} initialSlide={currentStep}>
+        <Carousel ref={sliderRef} initialSlide={currentStep} adaptiveHeight>
           <div>
             <Col span={12}>
               <Item
@@ -202,7 +203,105 @@ const _UserForm: FC<IUserForm> = ({
             </Col>
           </div>
           <div>
-            <h3>契約內容與免責聲明</h3>
+            <h3>
+              <Col span={24}>
+                <Item>
+                  <h1>
+                    1.
+                    您必須在此確認，關於身體狀況及使用健身器材的能力，在您加入之前無任何醫藥方面的建議，若目前有任何健康與醫療方面的任何顧慮，可能會影響您的健身計畫，請您在上課之前與專業醫師討論。
+                  </h1>
+                </Item>
+              </Col>
+              <Col span={24}>
+                <Item>
+                  {getFieldDecorator('checkbox-group', {
+                    initialValue: []
+                  })(
+                    <Group style={{ width: '100%' }}>
+                      <Checkbox value="A">
+                        <span>心臟疾病</span>
+                      </Checkbox>
+                      <Checkbox value="B">
+                        <span>低血壓 / 高血壓</span>
+                      </Checkbox>
+                      <Checkbox value="C">
+                        <span>低血糖 / 糖尿病</span>
+                      </Checkbox>
+                      <Checkbox value="D">
+                        <span>內分泌失調</span>
+                      </Checkbox>
+                      <Checkbox value="E">
+                        <span>暈眩</span>
+                      </Checkbox>
+                      <Checkbox value="F">
+                        <span>懷孕中 / 產後未超過一年</span>
+                      </Checkbox>
+                      <Checkbox value="G">
+                        <span>關節狀況 / 受傷</span>
+                      </Checkbox>
+                      <Checkbox value="H">
+                        <span>曾有重大手術</span>
+                      </Checkbox>
+                      <Checkbox value="I">
+                        <span>中風</span>
+                      </Checkbox>
+                      <Checkbox value="J">
+                        <span>上述未涉及之醫療狀況</span>
+                      </Checkbox>
+                    </Group>
+                  )}
+                </Item>
+              </Col>
+              <Col span={24}>
+                <Item>
+                  <h1>
+                    2.
+                    遵守現場教練指導，申請人在課程進行中隨時可以停止與休息，維護自己健康並未自身的健康問題承擔所有風險。
+                  </h1>
+                </Item>
+              </Col>
+              <Col span={24}>
+                <Item>
+                  <h1>
+                    3.
+                    需著運動服裝，拳擊區不可穿鞋，重訊區虛換乾淨運動鞋並攜帶毛巾。
+                  </h1>
+                </Item>
+              </Col>
+              <Col span={24}>
+                <Item>
+                  <h1>4. 管場內禁止飲食，水及運動飲料不在此限至內。</h1>
+                </Item>
+              </Col>
+              <Col span={24}>
+                <Item>
+                  <h1>
+                    5. 器材勿重摔，用必請歸回原位；惡意損毀器材者，將照價賠償。
+                  </h1>
+                </Item>
+              </Col>
+              <Col span={24}>
+                <Item>
+                  <h1>
+                    6.
+                    申請人在外地與他人發生鬥毆、打架等事件與本場館及教練無關。
+                  </h1>
+                </Item>
+              </Col>
+              <Col span={24}>
+                <Item>
+                  <h1>
+                    7.
+                    您在此確認已充分閱讀，理解和同意其中全部條款，即表示即日起同意本聲明內容。
+                  </h1>
+                </Item>
+              </Col>
+              <Col span={24}>
+                <Item>
+                  <h1>8. 野人格鬥體適能保有更改上述聲明事項內容之權利。</h1>
+                </Item>
+              </Col>
+            </h3>
           </div>
         </Carousel>
       </Form>
